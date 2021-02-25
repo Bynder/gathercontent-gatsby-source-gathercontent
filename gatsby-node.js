0729898ed731a4918899a35547edf97fb9ce0ad4
harperllama <kyle@kyleshrives.co.uk>
2021-02-25 15:28:39 +0000

    remove try

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,78 +7,73 @@ exports.onPostBuild = ({ reporter }) => {
 exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, options) => {
   const { createNode } = actions
   const { email, apiKey, projectId } = options
+  const { folders, items, templates, project } = await getProjectData(projectId, { apiKey, email })
 
-  try {
-    const { folders, items, templates, project } = await getProjectData(projectId, { apiKey, email })
+  folders.map(f => {
+    createNode({
+      id: createNodeId(f.uuid),
+      name: f.name,
+      slug: f.slug,
+      position: f.position,
+      parent: f.parentUuid ? createNodeId(f.parentUuid) : null,
+      children: items.filter(i => i.folderUuid === f.uuid).map(i => createNodeId(i.id)),
+      internal: {
+        type: 'gathercontentFolders',
+        content: JSON.stringify(f),
+        contentDigest: createContentDigest(f)
+      }
+    });
+  })
 
-    folders.map(f => {
-      createNode({
-        id: createNodeId(f.uuid),
-        name: f.name,
-        slug: f.slug,
-        position: f.position,
-        parent: f.parentUuid ? createNodeId(f.parentUuid) : null,
-        children: items.filter(i => i.folderUuid === f.uuid).map(i => createNodeId(i.id)),
-        internal: {
-          type: 'gathercontentFolders',
-          content: JSON.stringify(f),
-          contentDigest: createContentDigest(f)
-        }
-      });
+  items.map(i => {
+    createNode({
+      id: createNodeId(i.id),
+      itemId: i.id,
+      name: i.name,
+      slug: i.slug,
+      status: i.statusName,
+      itemContent: i.itemContent,
+      parent: i.folderUuid,
+      position: i.position,
+      children: [],
+      folder___NODE: createNodeId(i.folderUuid),
+      template___NODE: i.templateId ? createNodeId(i.templateId) : null,
+      status___NODE: i.statusId ? createNodeId(i.statusId) : null,
+      internal: {
+        type: 'gathercontentItems',
+        content: JSON.stringify(i),
+        contentDigest: createContentDigest(i),
+      },
     })
+  })
 
-    items.map(i => {
-      createNode({
-        id: createNodeId(i.id),
-        itemId: i.id,
-        name: i.name,
-        slug: i.slug,
-        status: i.statusName,
-        itemContent: i.itemContent,
-        parent: i.folderUuid,
-        position: i.position,
-        children: [],
-        folder___NODE: createNodeId(i.folderUuid),
-        template___NODE: i.templateId ? createNodeId(i.templateId) : null,
-        status___NODE: i.statusId ? createNodeId(i.statusId) : null,
-        internal: {
-          type: 'gathercontentItems',
-          content: JSON.stringify(i),
-          contentDigest: createContentDigest(i),
-        },
-      })
-    })
+  templates.map(t => {
+    createNode({
+      id: createNodeId(t.id),
+      name: t.name,
+      slug: t.slug,
+      parent: null,
+      children: items.filter(i => i.structureUuid === t.structureUuid).map(i => createNodeId(i.id)),
+      internal: {
+        type: 'gathercontentTemplates',
+        content: JSON.stringify(t),
+        contentDigest: createContentDigest(t)
+      }
+    });
+  })
 
-    templates.map(t => {
-      createNode({
-        id: createNodeId(t.id),
-        name: t.name,
-        slug: t.slug,
-        parent: null,
-        children: items.filter(i => i.structureUuid === t.structureUuid).map(i => createNodeId(i.id)),
-        internal: {
-          type: 'gathercontentTemplates',
-          content: JSON.stringify(t),
-          contentDigest: createContentDigest(t)
-        }
-      });
-    })
-
-    project.statuses.data.map(s => {
-      createNode({
-        id: createNodeId(s.id),
-        name: s.name,
-        slug: s.slug,
-        parent: null,
-        children: items.filter(i => i.statusId === s.id).map(i => createNodeId(i.id)),
-        internal: {
-          type: 'gathercontentStatuses',
-          content: JSON.stringify(s),
-          contentDigest: createContentDigest(s)
-        }
-      });
-    })
-  } catch(e) {
-    console.error('Failed to create nodes', e);
-  }
+  project.statuses.data.map(s => {
+    createNode({
+      id: createNodeId(s.id),
+      name: s.name,
+      slug: s.slug,
+      parent: null,
+      children: items.filter(i => i.statusId === s.id).map(i => createNodeId(i.id)),
+      internal: {
+        type: 'gathercontentStatuses',
+        content: JSON.stringify(s),
+        contentDigest: createContentDigest(s)
+      }
+    });
+  })
 }
